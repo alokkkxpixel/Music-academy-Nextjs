e4d192afd9c7011c35cc67e9089ec4c61be658c2
alokkkxpixel <alokpithale4@gmail.com>
2025-08-22 03:05:45 +0530

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import {  HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+
+
+
+
+const Navbar = ({ className }: { className?: string }) => {
+     const [active, setActive] = useState<string | null>(null);
+  return (
+    <div className="w-full px-20 py-5 flex items-center justify-center">
+
+        <Menu setActive={setActive} >
+
+        <Link   href="/">
+        <MenuItem setActive={setActive} active={active} item="Home" >
+        </MenuItem>
+        </Link>
+        <Link   href="/">
+        <MenuItem setActive={setActive} active={active} item="Our courses" >
+        <div className="flex flex-col space-y-4  text-sm">
+            
+           <HoveredLink href="/web-dev">Web Development</HoveredLink>
+            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+        </div>
+        </MenuItem>
+        </Link>
+          
+          <Link href='/contact' >
+          
+            <MenuItem setActive={setActive} active={active} item="Conatact Us" >
+          
+        </MenuItem>
+          </Link>
+        
+        </Menu>
+       
+    </div>
+  )
+}
+
+export default Navbar
